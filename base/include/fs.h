51e1f5839c1ab122644a3dd6104fccd0f8ce8b21
Lachlan Chun <lachlan.chun@student.uq.edu.au>
2025-05-24 01:48:21 +1000

    Working FS with user configuration.

---
+++ base/include/fs.h
@@ -0,0 +1,27 @@
+/*
+* @file     fs.h
+* @brief    File System Library
+* @author   Lachlan Chun, 47484874
+*/
+
+#ifndef FS_H
+#define FS_H
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/data/json.h>
+#include <zephyr/fs/fs.h>
+#include <zephyr/fs/littlefs.h>
+#include <zephyr/storage/flash_map.h>
+#include <zephyr/logging/log.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "user.h"
+
+#define STACK_SIZE 4096
+#define THREAD_PRIORITY 1
+
+extern void fs_init(void);
+
+#endif
