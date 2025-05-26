3dc80040f5e1bfa747a3f5b9055b9d272fc5725a
Kevin Gu <s4743888@student.uq.edu.au>
2025-05-27 09:43:21 +1000

    Add bounding box drawing to image

--- pc_software/recognition.py
+++ pc_software/recognition.py
@@ -3,10 +3,12 @@ A short script to detect objects using YOLOv8ls
 
 """
 from ultralytics import YOLO
+import cv2
 
 def detect_objects(image_path):
     # Saves a model called yolov8n.pt
     model = YOLO("yolov8n.pt")
+    model.fuse()  # Make faster
     results = model(image_path)
     names = model.names
 
@@ -14,7 +16,23 @@ def detect_objects(image_path):
     for box in results[0].boxes:
         cls_id = int(box.cls[0])
         conf = box.conf[0].item()
-        print(f"Detected: {names[cls_id]} with confidence {conf:.2f}")
+        label = f"{results[0].names[cls_id]}: {conf:.2f}"
+        
+        print(f"Detected: {label}")
+
+        x1, y1, x2, y2 = map(int, box.xyxy[0])  # bounding box
+
+        (label_width, label_height), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)
+        y = max(y1, label_height + 10)  # ensure label is inside image
+
+        # Draw rectangle and label
+        cv2.rectangle(results[0].orig_img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.putText(results[0].orig_img, label, (x1, y - 5),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # Save only once after drawing all boxes
+    cv2.imwrite("bounding_boxed.jpg", results[0].orig_img)
+
 
 if __name__ == "__main__":
     detect_objects("soccer_ball.jpg")
